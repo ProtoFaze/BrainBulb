80d3dd94ba324b20f18fac917f32b54a3ddbea73
ProtoFaze <110209956+ProtoFaze@users.noreply.github.com>
2023-04-10 23:32:31 +0800

    teacher profile WIP

--- src/views/ProfileParent.php
+++ src/views/ProfileParent.php
@@ -6,14 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="../../images/brainlogo3.png">
     <link rel="stylesheet" href="../styles/layout.css">
+    <link rel="stylesheet" href="../styles/inputs.css">
     <title>Parent Profile</title>
     <?php 
+    //load page
         include "../database/connect.php";
         if(session_status() == PHP_SESSION_NONE) {
             session_start();
         }
 
+
+
+
+
         $parent_id = 'PT00000001';
+
+
+
+
+
         // $_SESSION['parent_id'];
         $profileRequest = "SELECT * FROM parent 
         inner join user on parent.parent_ID = user.parent_ID
@@ -23,14 +34,23 @@
 
         $childsql = "SELECT * FROM student WHERE parent_ID = '$parent_id'";
         $childRequest = mysqli_query($connection, $childsql);
+
+    //redirect to child
+        if(isset($_POST['child'])){
+            $child_id = $_POST['child'];
+            $_SESSION['child_id'] = $child_id;
+            header("Location: ../views/ProfileChild.php");
+        }
     ?>
     <style>
+        main{
+            padding: 0px 120px;
+        }
         .content_box{
             height:300px;
             width: 100%;
             margin: 0 auto;
             padding: 0px 50px;
-
         }
         .row{
             align-self: stretch;
@@ -39,7 +59,15 @@
         .elipse_container{
             width: 100px;
             height: 100px;
-
+        }
+        a{
+            text-decoration: none;
+        }
+        /* .split_container{
+            height:100%;
+        } */
+        .split_section{
+            flex-grow: 1;
         }
     </style>
 </head>
@@ -48,29 +76,31 @@
         <?php include "../components/nav.php"; ?>
     </header>
     <main>
-        <!-- top section containing back button, username and edit button -->
+        <!-- top section containing back button, username_ID and edit button -->
         <div class="response">
-            <a href="mainpageParent.php"><button class="material-symbols-outlined flex_button">arrow_back_ios</button></a>
-            <h1>Parent Name</h1>
-            <a href="editParent.php"><button class="material-symbols-outlined flex_button">edit</button></a>
+            <a href="mainpageParent.php"><button class="flex_button"><span class="material-symbols-outlined">arrow_back_ios</span>Go Back</button></a>
+            <h1><?= $parent['parent_ID']?>'s profile</h1>
+            <a href="editParent.php"><button class="flex_button"><span class="material-symbols-outlined">edit</span>Edit some information</button></a>
         </div>
+
         <!-- middle section containing user info, parent into -->
         <div class="split_container">
             <div class="split_section">
-                <img class="elipse_container"src="" alt="parent picture">
-                <div class="info_ltr">Number of children <!--<h2 stye = "display:inline;">--><?= mysqli_num_rows($childRequest)?><!--</h2>--></div>
-                <div class="info_ltr">Name <?= $parent['pName']?></div>
-                <div class="info_ltr">Birthdate <?= $parent['pDOB']?></div>
+                <img class="elipse_container"src="<?= $parent['profile_Picture']?>" alt="parent picture">
+                <div class="info_ltr"><h3>Number of children</h3><div class="divider">:</div><p><?= mysqli_num_rows($childRequest)?></p></div>
+                <div class="info_ltr"><h3>Name</h3><div class="divider">:</div><p><?=$parent['pName']?></p></div>
+                <div class="info_ltr"><h3>birthdate</h3><div class="divider">:</div><p><?= $parent['pDOB']?></p></div>
             </div>
             <div class="split_section">
-                <div class="info_ltr">Username <?= $parent['username']?></div>
-                <div class="info_ltr">Email <?= $parent['email']?></div>
-                <div class="info_ltr">IC Number <?= $parent['ic']?></div>
+                <div class="info_ltr"><h3>Username</h3> <div class="divider">:</div><p><?= $parent['username']?></p></div>
+                <div class="info_ltr"><h3>Email</h3> <div class="divider">:</div><p><?= $parent['email']?></p></div>
+                <div class="info_ltr"><h3>IC Number</h3> <div class="divider">:</div><p><?= $parent['ic']?></p></div>
             </div>
         </div>
-        <!-- bottomSection containing children info -->
+
+        <!-- bottom section containing children info -->
         <div class="content_box">
-        <?php if(mysqli_num_rows($childRequest) > 0)
+        <?php if(mysqli_num_rows($childRequest) > 0){
             while ($child = mysqli_fetch_assoc($childRequest)) {
             echo <<<HTML
                 <div class="row">
@@ -78,9 +108,13 @@
                     <div class="info_ltr">$child[sName]</div>
                     <div class="info_ltr">Grade : $child[sGrade]</div>
                     <div class="info_ltr">Streak: $child[aFrequency]</div>
+                    <form action="window.location.href = 'application_details.php'" method="post">
+                        <input type="hidden" name="child" value="$child[student_ID]">
+                        <button class="materials-symbols-outlined flex_button" type="submit">More Details<span class="material-symbols-outlined">arrow_forward_ios</span></button>
+                    </form>
                 </div>
             HTML;
-            }else{
+        }}else{
                 echo "<h2> No children found </h2>";
             }?>
         </div>
---
+++ src/views/ProfileTeacher.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="../../images/brainlogo3.png">
+    <link rel="stylesheet" href="../styles/layout.css">
+    <link rel="stylesheet" href="../styles/inputs.css">
+    <title>Parent Profile</title>
+    <?php 
+    //load page
+        include "../database/connect.php";
+        if(session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+
+
+
+
+        $teacher_id = 'TC00000001';
+
+
+
+
+
+        // $_SESSION['parent_id'];
+        $profileRequest = "SELECT * FROM teacher 
+        inner join user on teacher.teacher_ID = user.teacher_ID
+        WHERE teacher.teacher_ID = '$teacher_id'";
+        $teacherRequest = mysqli_query($connection, $profileRequest);
+        $teacher = mysqli_fetch_assoc($teacherRequest);
+
+
+    //redirect to child
+        if(isset($_POST['teacher_id'])){
+            $teacher_id = $_POST['teacher_id'];
+            $_SESSION['teacher_id'] = $teacher_id;
+            echo "<script>window.location.href='materialsUpload.php'</script>";
+        }
+    ?>
+    <style>
+        main{
+            padding: 0px 120px;
+        }
+        .content_box{
+            height:300px;
+            width: 100%;
+            margin: 0 auto;
+            padding: 0px 50px;
+        }
+        .row{
+            align-self: stretch;
+            height: 30%;
+        }
+        .elipse_container{
+            width: 100px;
+            height: 100px;
+        }
+        a{
+            text-decoration: none;
+        }
+        .split_section{
+            flex-grow: 1;
+        }
+        .user_info,.qualifications{
+            display: inherit;
+            flex-direction: column;
+            align-self: stretch;
+            border-radius: inherit;
+            padding: 15px 30px;
+            gap: inherit;
+        }
+        .heading_and_data>p{
+            border-bottom: 3px solid black;
+            border-left: 1px solid black;
+            border-bottom-left-radius: var(--border-radius);
+            padding: 0px 10px 0px 20px;
+        }
+        .heading_and_data,.heading_and_data>h3{
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <?php include "../components/nav.php"; ?>
+    </header>
+    <main>
+        <!-- top section containing back button, username_ID and edit button -->
+        <div class="response">
+            <a href="mainpageTeacher.php"><button class="flex_button"><span class="material-symbols-outlined">arrow_back_ios</span>Go Back</button></a>
+            <h1><?= $teacher['teacher_ID']?>'s profile</h1>
+            <a href="editTeacher.php"><button class="flex_button"><span class="material-symbols-outlined">edit</span>Edit some information</button></a>
+        </div>
+
+        <!-- middle section containing user info, teacher into -->
+        <div class="split_container">
+            <div class="split_section">
+                <img class="elipse_container"src="<?=$teacher['profile_Picture']?>" alt="teacher picture">
+                <div class="info_ltr"><h3>Name</h3><div class="divider">:</div><p><?=$teacher['tName']?></p></div>
+                <div class="info_ltr"><h3>Birthdate</h3><div class="divider">:</div><p><?= $teacher['tDOB']?></p></div>
+                <div class="info_ltr"><h3>School</h3><div class="divider">:</div><p><?= $teacher['tSchool']?></p></div>
+                <div class="info_ltr"><h3>Region</h3><div class="divider">:</div><p><?= $teacher['tRegion']?></p></div>
+            </div>
+            <div class="split_section" style="background-color: var(--bg); padding: 0;">
+                <div class="user_info" style="background-color: var(--box-primary);">
+                    <div class="info_ltr"><h3>Username</h3> <div class="divider">:</div><p><?= $teacher['username']?></p></div>
+                    <div class="info_ltr"><h3>Email</h3> <div class="divider">:</div><p><?= $teacher['email']?></p></div>
+                    <div class="info_ltr"><h3>IC Number</h3> <div class="divider">:</div><p><?= $teacher['ic']?></p></div>
+                </div>
+                <div class="qualifications" style="background-color: var(--box-primary);">
+                    <div class="heading_and_data"><h3>Qualifications</h3><br/><p><?=$teacher['highest_Qualification']?></p></div>
+                    <div class="heading_and_data"><h3>Certification</h3><br/><p><?=$teacher['professionality_Description']?></p></div>
+                </div>
+            </div>
+        </div>
+
+        <!-- bottom section containing children info -->
+        <!-- <div class="content_box">
+        </div> -->
+    </main>
+</body>
+</html>

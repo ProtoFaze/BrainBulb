8459925e99738bf734aa6214619976e83eb3f762
RONG WEI <horongwei0110@gmail.com>
2023-05-02 02:55:37 +0800

    HRW

--- src/views/DiscussionPlatform.php
+++ src/views/DiscussionPlatform.php
@@ -1,200 +1,234 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Discussion Platform</title>
-    <link rel="stylesheet" href="../styles/design.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@40,500,0,0" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@40,500,0,0" />
-    <style>
-        body{
-            background-color:#f2f5f7 ;
-        }
-
-        .titleBox >h1 {
-            color:#f2f5f7 ;
-        }
-
-        h2,h4,.rules {
-            font-family: 'Open Sans', sans-serif;
-            margin-top: 10px;
-            margin-bottom: 0;
-        }
-
-        .container {
-            max-width: max;
-            margin: 0 auto;
-            padding: 20px;
-        }
-
-        #dateTime {
-            font-family: Arial, Helvetica, sans-serif;
-            font-size: 14px;
-            font-style: italic;
-            color:#808080 ;
-            padding: 0;
-        }
-
-        .discussion {
-            display: flex;
-            flex-direction: row;
-            padding: 20px;
-            width: 1500px;
-            margin-left: auto;
-            margin-right: auto;
-            height: fit-content;
-            background-color:#ffffff;
-            border-radius: 4px;
-            position: relative;
-            /* https://css-tricks.com/almanac/properties/b/box-shadow/ */
-            box-shadow: 0 8px 8px -4px rgb(202, 194, 194);
-        }
-
-        .left {
-            flex-basis: 18%;
-            text-align: center;
-        }
-
-        .left img {
-            width: 100%;
-            max-width: 110px;
-            border-radius: 50%;
-        }
-
-        .right {
-            flex-basis: 80%;
-            margin-left: 20px;
-        }
-
-        .right h2 {
-            font-size: 24px;
-            margin-bottom: 10px;
-            margin-top: 0px;
-        }
-
-        .tagsAndbuttons {
-            position: absolute;
-            bottom: 0;
-            right: 0;
-        }
-
-        .tagsAndbuttons button,span {
-            background-color: #3a4b61;
-            color: #f1f4f3;
-            padding: 10px 20px;
-            border-radius: 10px;
-            border: none;
-            margin-right: 18px;
-            margin-bottom: 20px;
-            cursor: pointer;
-            font-size: 18px;
-            display: flex;
-            flex-direction: row;
-            flex-wrap: wrap;
-        }
-
-
-        #reminders {
-            font-size: 14px;
-            margin-left: auto;
-            color: #999;
-        }
-
-        .question{
-            margin-bottom: 15px;
-        }
-
-        .material-symbols-outlined {
-            margin: 0;
-            padding: 0;
-            margin-right: 15px;
-        }
-
-        .tagsAndbuttons{
-            display: flex;
-            flex-direction: row;
-        }
-
-        .textBesideIcon{
-            margin-top: 1.5px;
-        }
-
-        .ask-Btn, .sort-Btn {
-            background-color: #3a4b61;
-            border: none;
-            color: #f1f4f3;
-            padding: 10px 20px;
-            text-align: left;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;
-            border-radius: 5px;
-            cursor: pointer;
-            box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
-
-        }
-
-        .functionButton{
-            margin-left: 76%;
-        }
-
-        .ask-Btn{
-            text-align: center;
-        }
-    </style>
-</head>
-
-<body>
-    <?php
-        include("../components/nav.php");
-        include "../database/connect.php";
-    ?>
-    <div class="titlebox">
-        <h1>QNA Discussion</h1>
-    </div>
-    <div class="functionButton">
-        <button class="ask-Btn">Ask Query</button>
-        <select class="sort-Btn">
-            <option value="mostLikes">Most Likes</option>
-            <option value="mostComments">Most Comments</option>
-            <option value="latestPost">Latest Post</option>
-        </select>
-    </div>
-    <div class="container">
-		<div class="discussion">
-			<div class="left">
-                <img src="../../images/anonymousUser.png">
-				<h4>Muhammad Tambi Alamaks Thuhan</h4>
-				<p id="dateTime">10th January 2023 13:49:32</p>
-			</div>
-			<div class="right">
-                <div class="question">
-                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nisi magni praesentium harum obcaecati reiciendis fugiat dolorum itaque dolore? Veritatis labore voluptatum perspiciatis nulla dolorem eaque id deserunt ab quos vero, doloribus corporis impedit provident sapiente non rem eius aliquid quasi! Laboriosam sint tempore rem, ratione voluptas repudiandae veniam unde.</h2>
-                </div>
-				<div class="tagsAndbuttons">
-                    <span>#TestingLongText</span>
-					<button id="likebtn">
-                        <span class="material-symbols-outlined">
-                            favorite
-                        </span>
-                        <div class="textBesideIcon">
-                            210 Likes
-                        </div>
-                    </button>
-					<button id="replybtn">
-                        <span class="material-symbols-outlined">
-                            chat
-                        </span>
-                        <div class="textBesideIcon">
-                            10 Replies
-                        </div>
-                    </button>
-				</div>
-			</div>
-		</div>
-	</div>
+<!-- <?php
+    session_start();
+    ?> -->
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Discussion Platform</title>
+        <link rel="stylesheet" href="../styles/design.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@40,500,0,0" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@40,500,0,0" />
+        <style>
+            body{
+                background-color:#f2f5f7 ;
+            }
     
-</body>
-</html>
+            .titleBox >h1 {
+                color:#f2f5f7 ;
+            }
+    
+            h2,h4,.rules {
+                font-family: 'Open Sans', sans-serif;
+                margin-top: 10px;
+                margin-bottom: 0;
+            }
+    
+            .container {
+                max-width: max;
+                margin: 0 auto;
+                padding: 20px;
+            }
+    
+            #dateTime {
+                font-family: Arial, Helvetica, sans-serif;
+                font-size: 14px;
+                font-style: italic;
+                color:#808080 ;
+                padding: 0;
+            }
+    
+            .discussion {
+                padding: 20px;
+                max-width: 1500px;
+                margin-left: auto;
+                margin-right: auto;
+                background-color:#ffffff;
+                border-radius: 4px;
+                position: relative;
+                /* https://css-tricks.com/almanac/properties/b/box-shadow/ */
+                box-shadow: 0 8px 8px -4px rgb(202, 194, 194);
+            }
+    
+            .left {
+                display: flex;
+                flex-direction: row;
+            }
+    
+            .left img ,.img{
+                width: 50px;
+                height: 50px;
+                background-color: #3a4b61;
+            }
+    
+            .left div {
+                margin: 8px;
+            }
+    
+            
+    
+            .right h2 {
+                font-size: 20px;
+                margin-bottom: 60px;
+                margin-top: 0px;
+            }
+    
+            .tagsAndbuttons {
+                position: absolute;
+                bottom: 0;
+                right: 0;
+            }
+    
+            .tagsAndbuttons button,span {
+                background-color: #3a4b61;
+                color: #f1f4f3;
+                padding: 10px 20px;
+                border-radius: 10px;
+                border: none;
+                margin-right: 18px;
+                margin-bottom: 20px;
+                cursor: pointer;
+                font-size: 18px;
+                display: flex;
+                flex-direction: row;
+                flex-wrap: wrap;
+            }
+    
+    
+            /* #reminders {
+                font-size: 14px;
+                margin-left: auto;
+                color: #999;
+            } */
+    
+            .question{
+                /* margin-bottom: 15px; */
+                margin: 25px;
+            }
+    
+            .material-symbols-outlined {
+                margin: 0;
+                padding: 0;
+                margin-right: 15px;
+            }
+    
+            .tagsAndbuttons{
+                display: flex;
+                flex-direction: row;
+            }
+    
+            .textBesideIcon{
+                margin-top: 1.5px;
+            }
+    
+            .ask-Btn, .sort-Btn {
+                background-color: #3a4b61;
+                border: none;
+                color: #f1f4f3;
+                padding: 10px 20px;
+                text-align: left;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                border-radius: 5px;
+                cursor: pointer;
+                box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
+            }
+    
+            .functionButton{
+                margin-left: 76%;
+            }
+    
+            .ask-Btn{
+                text-align: center;
+            }
+    
+            #replybtn, .tagsAndbuttons>span{
+                cursor: context-menu;
+            }
+
+            #sNameATime{
+                padding-top: 5px;
+            }
+
+            .TopicWithLink{
+            color: black;
+            font-weight: bold;
+            text-decoration: none; /* no underline */
+            }
+
+        </style>
+    </head>
+    
+    <body>
+        <?php
+            include("../components/nav.php");
+            include "../database/connect.php";
+        ?>
+        <div class="titlebox">
+            <h1>QNA Discussion</h1>
+        </div>
+        <div class="functionButton">
+            <a class="askQueryBtn" href="PostQuery.php"><button class="ask-Btn">Ask Query</button></a>
+            <select class="sort-Btn">
+                <option value="mostLikes">Most Likes</option>
+                <option value="mostComments">Most Comments</option>
+                <option value="latestPost">Latest Post</option>
+            </select>
+        </div>
+        <?php
+            $sql_ShowQuery = "SELECT d.query_ID AS queryID, d.title AS topic, d.tags AS tagline, d.post_Query_Datetime AS qDateTime, d.query_Likes AS likes, s.sName AS name, u.profile_Picture AS pic, 
+            (SELECT COUNT(*) FROM blogreplies WHERE query_ID = d.query_ID) AS totalQuery FROM discussion d INNER JOIN student s ON d.student_ID = s.student_ID INNER JOIN user u ON s.student_ID = u.student_ID ORDER BY d.post_Query_Datetime DESC";     
+            $result_ShowQuery = mysqli_query($connection,$sql_ShowQuery);
+    
+    if(mysqli_num_rows($result_ShowQuery) > 0){
+        while($row = mysqli_fetch_assoc($result_ShowQuery)){
+            echo <<<HTML
+            <div class="container">
+                <div class="discussion">
+                    <div class="left">
+                        <div class="img">
+                            <img src="../../images/anonymousUser.png">
+                        </div>
+                        <div id="sNameATime"><h4>{$row["name"]}</h4></div>
+                        <div id="sNameATime"><p id="dateTime">{$row["qDateTime"]}</p></div>
+                    </div>
+                    <div class="right">
+                        <div class="question">
+                            <a class="TopicWithLink" href="viewSelectedQueries.php?id={$row["queryID"]}" id="{$row["queryID"]}"><h2>{$row["topic"]}</h2></a>
+                        </div>
+                    </div>
+                    <div class="tagsAndbuttons">
+                        <span>{$row["tagline"]}</span>
+                        <button id="likebtn">
+                            <span class="material-symbols-outlined">
+                                favorite
+                            </span>
+                            <div class="textBesideIcon">
+                                {$row["likes"]} Likes
+                            </div>
+                        </button>
+                        <button id="replybtn">
+                            <span class="material-symbols-outlined">
+                                chat
+                            </span>
+                            <div class="textBesideIcon">
+                                {$row["totalQuery"]} Replies
+                            </div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            HTML;
+                }
+            }
+            else{
+                echo "No data";
+            }
+            ?>
+        
+        
+    </body>
+    </html>
--- src/views/viewSelectedQueries.php
+++ src/views/viewSelectedQueries.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+    $queryID = $_SESSION['queryID'];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -45,7 +49,7 @@
             margin-left: auto;
             margin-right: auto;
             margin-top: 1%;
-            height: fit-content;
+            height: auto;
             background-color:#ffffff;
             border-radius: 4px;
             position: relative;
@@ -56,6 +60,8 @@
         .left {
             flex-basis: 18%;
             text-align: center;
+            margin-top: 1%;
+            height:fit-content;
         }
 
         .left img {
@@ -67,6 +73,8 @@
         .right {
             flex-basis: 80%;
             margin-left: 20px;
+            margin-top: 8px;
+            height:fit-content;
         }
 
         .right h2 {
@@ -114,10 +122,10 @@
         }
 
         .textBesideIcon{
-            margin-top: 1.5px;
+            margin-top: 1px;
         }
 
-        .ask-Btn, .sort-Btn {
+        .reply-Btn{
             background-color: #3a4b61;
             border: none;
             color: #f1f4f3;
@@ -129,18 +137,26 @@
             border-radius: 5px;
             cursor: pointer;
             box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
+            width: 41%;
+            height: 40px;
+            text-align: center;
+        }
 
+        .reply-Btn:hover{
+            background-color: #2c3e50 ;
+            width: 43%;
+            height: 43px;
         }
 
         .functionButton{
-            margin-left: 76%;
+            margin-left: 81%;
         }
 
         .ask-Btn{
             text-align: center;
         }
 
-        .like-btn, .userRole-btn{
+        .like-btn, .userRole-btn, .taglineBtn{
             display: flex;
             flex-direction: row;
             margin-bottom: 5%;
@@ -152,10 +168,9 @@
             border: none;
             cursor: pointer;
             font-size: 18px;
-            height: 12%;
+            height: 10px;
             width: 74%;
             padding-bottom: 12%;
-
         }
 
         .userRole-btn{
@@ -165,12 +180,76 @@
 
         .replyContainer{
             margin-left: 11%;
-            margin-top: 2%;
-            margin-bottom: 0%;
             font-size: 26px;
         }
+        
+        /* ReplyBlog CSS */
+        .replyBlock {
+            margin-top: 20px;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            background-color: #f7f7f7;
+            width: 1500px;
+            height: fit-content;
+            margin-left: auto;
+            margin-right: auto;
+        }
 
+        .replyBlock > h4 {
+            font-size: 16px;
+            font-weight: bold;
+            margin-bottom: 10px;
+            margin-left: 0%;
+        }
+
+        .replyBlock textarea {
+            display: block;
+            width: 100%;
+            padding: 10px;
+            font-family: 'Open Sans', sans-serif;
+            font-size: 15px;
+            margin-top: 1%;
+            margin-bottom: 1%;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        .replyBlock button {
+            display: inline-block;
+            margin-right: 10px;
+            padding: 10px;
+            border: none;
+            border-radius: 5px;
+            background-color: #ccc;
+            color: #000000;
+            font-weight: bolder;
+            cursor: pointer;
+        }
+
+        .replyBlock button:hover {
+            background-color: #555;
+            color: #fff;
+        }
+        
+        .postAndCancel{
+            display: flex;
+            flex-direction: row;
+            margin-left: 91%;
+        }
+
+        .taglineBtn{
+            padding-left: 30px;
+            padding-top: 13px;
+            margin-top: 0;
+            cursor: context-menu;
+        }
+
+        button > span {
+            background-color: transparent;
+        }
     </style>
+    
 </head>
 
 <body>
@@ -182,115 +261,121 @@
         <h1>QNA Discussion</h1>
     </div>
     <div class="functionButton">
-        <button class="ask-Btn">Ask Query</button>
-        <select class="sort-Btn">
-            <option value="mostLikes">Most Likes</option>
-            <option value="mostComments">Most Comments</option>
-            <option value="latestPost">Latest Post</option>
-        </select>
+        <button class="reply-Btn" id="replyButton">REPLY</button>
     </div>
-
     <div class="container">
         <div class="discussion">
                 <div class="left">
                     <img src="../../images/anonymousUser.png">
                     <h4>Muhammad Tambi Alamaks Thuhan</h4>
                     <p id="dateTime">10th January 2023 13:49:32</p>
-                
                     <button class="like-btn">
-                            <span class="material-symbols-outlined">
-                                favorite
-                            </span>
-                            <div class="textBesideIcon">
-                                0 Likes
-                            </div>
+                        <span class="material-symbols-outlined">
+                            favorite
+                        </span>
+                        <div class="textBesideIcon">
+                            0 Likes
+                        </div>
+                    </button>
+                    <button class="taglineBtn">
+                        GROUP123455
                     </button>
                 </div>
                 <div class="right">
                     <div class="question">
                         <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nisi magni praesentium harum obcaecati reiciendis fugiat dolorum itaque dolore? Veritatis labore voluptatum perspiciatis nulla dolorem eaque id deserunt ab quos vero, doloribus corporis impedit provident sapiente non rem eius aliquid quasi! Laboriosam sint tempore rem, ratione voluptas repudiandae veniam unde.</h2>
-                    </div>
-                    <div class="tagsAndbuttons">
-                        <span>How to solve 1+1</span>
-                    </div>
+                    </div>               
                 </div>
         </div>
     </div>
+    
     <div class="replyContainer">
         Replies
     </div>
+
+    <div class="replyBlock" id="hideReply" style="display:none">
+        <h4>RONG WEIWEI<h4>
+        <form action="" method="post">
+            <textarea name="questionInput" id="" cols="30" rows="8"></textarea>
+            <div class="postAndCancel">
+                <button id="cancelBtn">Cancel</button>
+                <button name="postQuery">Post</button>
+            </div>
+        </form>
+    </div>
+
     <div class="container">
-		<div class="discussion">
-			<div class="left">
+        <div class="discussion">
+            <div class="left">
                 <img src="../../images/anonymousUser.png">
-				<h4>Muhammad Tambi Alamaks Thuhan</h4>
-				<p id="dateTime">10th January 2023 13:49:32</p>
+                <h4>Muhammad Tambi Alamaks Thuhan</h4>
+                <p id="dateTime">10th January 2023 13:49:32</p>
                 <button class="userRole-btn">
-                        <span class="material-symbols-outlined">
-                            group
-                        </span>
-                        <div class="textBesideIcon">
-                            Teacher Reply
-                        </div>
+                    <span class="material-symbols-outlined">
+                        group
+                    </span>
+                    <div class="textBesideIcon">
+                        Teacher Reply
+                    </div>
                 </button>
                 <button class="like-btn">
-                        <span class="material-symbols-outlined">
-                            favorite
-                        </span>
-                        <div class="textBesideIcon">
-                            0 Likes
-                        </div>
+                    <span class="material-symbols-outlined">
+                        favorite
+                    </span>
+                    <div class="textBesideIcon">
+                        0 Likes
+                    </div>
                 </button>
-			</div>
-			<div class="right">
+            </div>
+            <div class="right">
                 <div class="question">
                     <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nisi magni praesentium harum obcaecati reiciendis fugiat dolorum itaque dolore? Veritatis labore voluptatum perspiciatis nulla dolorem eaque id deserunt ab quos vero, doloribus corporis impedit provident sapiente non rem eius aliquid quasi! Laboriosam sint tempore rem, ratione voluptas repudiandae veniam unde.</h2>
                 </div>
-			</div>
-		</div>
+            </div>
+        </div>
 	</div>
-    <div class="discussion">
-			<div class="left">
-                <img src="../../images/anonymousUser.png">
-				<h4>Muhammad Tambi Alamaks Thuhan</h4>
-				<p id="dateTime">10th January 2023 13:49:32</p>
-               
-                <button class="like-btn">
-                        <span class="material-symbols-outlined">
-                            favorite
-                        </span>
-                        <div class="textBesideIcon">
-                            0 Likes
-                        </div>
-                </button>
-			</div>
-			<div class="right">
-                <div class="question">
-                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nisi magni praesentium harum obcaecati reiciendis fugiat dolorum itaque dolore? Veritatis labore voluptatum perspiciatis nulla dolorem eaque id deserunt ab quos vero, doloribus corporis impedit provident sapiente non rem eius aliquid quasi! Laboriosam sint tempore rem, ratione voluptas repudiandae veniam unde.</h2>
-                </div>
-			</div>
-    </div>
-    <div class="discussion">
-			<div class="left">
-                <img src="../../images/anonymousUser.png">
-				<h4>Muhammad Tambi Alamaks Thuhan</h4>
-				<p id="dateTime">10th January 2023 13:49:32</p>
-               
-                <button class="like-btn">
-                        <span class="material-symbols-outlined">
-                            favorite
-                        </span>
-                        <div class="textBesideIcon">
-                            0 Likes
-                        </div>
-                </button>
-			</div>
-			<div class="right">
-                <div class="question">
-                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nisi magni praesentium harum obcaecati reiciendis fugiat dolorum itaque dolore? Veritatis labore voluptatum perspiciatis nulla dolorem eaque id deserunt ab quos vero, doloribus corporis impedit provident sapiente non rem eius aliquid quasi! Laboriosam sint tempore rem, ratione voluptas repudiandae veniam unde.</h2>
-                </div>
-			</div>
-    </div>
 </div>
+    <script>
+            const targetDiv = document.getElementById("hideReply");
+            const btn = document.getElementById("replyButton");
+            const cancel = document.getElementById("cancelBtn");
+            btn.onclick = function () {
+            if (targetDiv.style.display !== "none") {
+                targetDiv.style.display = "none";
+            } else {
+                targetDiv.style.display = "block";
+            }
+            };
+            cancel.onclick = function () {
+            if (targetDiv.style.display == "block") {
+                targetDiv.style.display = "none";
+            } else {
+                targetDiv.style.display = "none";
+            }
+            };  
+    </script>
+
+    <?php
+    if(isset($_POST['postQuery'])){
+        if(isset($_SESSION['user_id'])){
+            $user_id = $_SESSION['user_id'];
+            $question = $_POST['questionInput'];
+            $date = date("Y-m-d H:i:s");
+            $sql = "INSERT INTO `qna`(`user_id`, `question`, `date`) VALUES ('$user_id','$question','$date')";
+            $result = mysqli_query($conn,$sql);
+            if($result){
+                echo "<script>alert('Question Posted!')</script>";
+                echo "<script>window.location.href='qna.php'</script>";
+            }else{
+                echo "<script>alert('Question Failed to Post!')</script>";
+                echo "<script>window.location.href='qna.php'</script>";
+            }
+        }else{
+            echo "<script>alert('No ID here')</script>";
+            echo "<script>window.location.href='qna.php'</script>";
+    }
+}
+        
+    ?>
 </body>
 </html>

41a6f71996dd48b3ab0857c2c508bac0eceef05f
Ojijibala <107234719+Ojijibala@users.noreply.github.com>
2023-05-10 08:10:00 +0800

    Merge branch 'main' of https://github.com/ProtoFaze/BrainBulb

--- src/views/setQuestion.php
+++ src/views/setQuestion.php
@@ -1,18 +1,6 @@
-<!-- $question
-$course_ID
-
-INSERT INTO questionCorrectAnswer (correct_List_ID,option1, option2,...)
-VALUES (correct_ID,...,...,...)
-
-INSERT INTO questionOptionList (option_List_ID,option1, option2,...)
-VALUES (correct_ID,...,...,...)
-
-INSERT INTO questionBank (course_ID, correct_list_ID, option_List_ID, question) 
-VALUES (courseID,correctID,optionID,questions);
-
-INSERT INTO table_name (column1, column2, column3, ...)
-VALUES (value1, value2, value3, ...); -->
-
+<?php
+    session_start();
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -24,7 +12,7 @@ VALUES (value1, value2, value3, ...); -->
 
     <style>
         body {
-            
+            background-image: linear-gradient(to bottom, white, #e6e9f9);
         }
         #setQues {
             background-color: rgba(255, 255, 255, 0.2);
@@ -55,53 +43,211 @@ VALUES (value1, value2, value3, ...); -->
             width: 120px;
             height: 35px;
             margin-right: 10px;
+            background-color: transparent;
+            border: 1.5px solid;
+            transition: background-color 1s;
+        }
+        .btn:hover {
+            background-color: rgb(50, 50, 50);
+            color: white;;
         }
         #btnBlock {
             margin-bottom: 10px;
+            float: right;
             /* position: absolute; */
+
         }
         #block {
-            height: 100%;
-            
+            /* height: 100%; */
+            /* clear: right; */
+            /* clear: left; */
+            position: relative;
+        }
+        #cancelBtn {
+            float: left;
+            margin-bottom: 10px;
+            margin-left: 0;
         }
     </style>
 </head>
 <body>
     <div id="setQues">
-        <!-- <h4>haha</h4> -->
-        <h2>Quiz Name</h2>
+        <h4><?php echo $quizName ?></h4>
         <hr>
         <div id="block">
-            <form action="">
+            <form action="" method="post">
                 <div id="ques">
-                    <p>Question 1</p>
-                    <input type="text" name="" id="quesInput">
+                    <p>Question </p><?php echo $_SESSION['pageNumber'] ?>
+                    <input type="text" name="ques" id="quesInput">
                 </div>
-
                 <div id="optionList">
                     <div class="options">
-                        <label for="op1">Option 1    </label><input type="checkbox" name="op1"><br>
+                        <label for="op1">Correct Answer</label>
+                        <br>
                         <input type="text" name="ans1" class="">
                     </div>
                     <div class="options">
-                        <label for="op2">Option 2    </label><input type="checkbox" name="op2"><br>
+                        <label for="op1">Wrong Answer</label>
+                        <br>
                         <input type="text" name="ans2" class="">
                     </div>
                     <div class="options">
-                        <label for="op3">Option 3    </label><input type="checkbox" name="op3"><br>
+                        <label for="op1">Wrong Answer</label>
+                        <br>
                         <input type="text" name="ans3" class="">
                     </div>
                     <div class="options">
-                        <label for="op4">Option 4    </label><input type="checkbox" name="op4"><br>
+                        <label for="op1">Wrong Answer</label>
+                        <br>
                         <input type="text" name="ans4" class="">
                     </div>
                 </div>
+                <button class="btn" name="done" type="submit">DONE</button>
+                <button class="btn" name="newQ">NEW QUESTION</button>
+                <button class="btn" id="cancelBtn" name="cancelBtn">CANCEL</button>
             </form>
         </div>
-        <div id="btnBlock">
-            <button class="btn">NEXT</button>
-            <button class="btn">CANCEL</button>
-        </div>
+        <!-- <form action="" method='post'>
+            
+        </form> -->
+        
     </div>
+    
 </body>
 </html>
+
+<?php
+    $quizName = $_GET['quizName'];
+    
+    // $query1 = "SELECT course_ID FROM course WHERE chapter_name = '$quizName'";
+    // $result = mysqli_query($connection, $query);
+    // if (mysqli_num_rows($result) > 0) {
+    //     while($row = mysqli_fetch_assoc($result)) {
+    //         $courseid = $row['chapter_name'];
+    //     }
+    // } else {
+    //     echo "0 results";
+    // }
+
+    $quizName = 'Quiz balala';
+    $courseid = 'CR00000034';
+
+    if(isset($_POST['done'])) {
+        arrayStoring();
+        insertQues();
+    }
+
+    if(isset($_POST['newQ'])) {
+        arrayStoring();
+        newPage();
+    }
+    
+    function newPage() {
+        // $pageNumber = isset($_SESSION['pageNumber']) ? $_SESSION['pageNumber'] : 1;
+        $pageNumber = isset($_SESSION['pageNumber']) && $_SESSION['pageNumber'] > 0 ? $_SESSION['pageNumber'] : 1;
+        // $pageNumber = $_SESSION['pageNumber'];
+        $pageNumber+=1;
+        $_SESSION['pageNumber'] = $pageNumber;
+        echo $_SESSION['pageNumber'];
+        header('Location: ' . $_SERVER['PHP_SELF']);
+        exit;
+    }
+    
+    
+    function arrayStoring() {
+        $ques = $_POST['ques'];
+        $correct = $_POST['ans1'];
+        $option1 = $_POST['ans2'];
+        $option2 = $_POST['ans3'];
+        $option3 = $_POST['ans4'];
+
+        $answers = array();
+        array_push($answers,$ques,$correct,$option1,$option2,$option3);
+        // $_SESSION['ansArray'][] = $answers;
+        array_push( $_SESSION['ansArray'],$answers);
+        print_r($_SESSION['ansArray']);
+        echo "Current page number: ".$_SESSION['pageNumber']."<br>";
+        return true;
+    }
+    
+
+    function insertQues() {
+        include 'dbcon.php';
+        // $_SESSION['ansArray'] = array();
+
+        // if(!isset($_SESSION['ansArray'])) {
+        //     return;
+        // }
+        foreach ($_SESSION['ansArray'] as $data ) {
+            // echo $data;
+            $questions = $data[0];
+            $correct = $data[1];
+            $option1 = $data[2];
+            $option2 = $data[3];
+            $option3 = $data[4];
+
+            $query2 = "INSERT INTO questioncorrectanswer (coption1)
+            VALUES ('$correct');";
+            
+            if (mysqli_query($connection, $query2)) {
+            } else {
+                echo "Error" . mysqli_error($connection);
+            }
+
+            $query3 = "INSERT INTO questionoptionlist (option1,option2,option3)
+            VALUES ('$option1','$option2','$option3');";
+            if (mysqli_query($connection, $query3)) {
+            } else {
+                echo "Error" . mysqli_error($connection);
+            }
+            
+            $query4 = "SELECT correct_List_ID FROM questioncorrectanswer ORDER BY correct_List_ID DESC LIMIT 1;";
+            $result = mysqli_query($connection, $query4);
+            if (mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_assoc($result)) {
+                    $correctID = $row['correct_List_ID'];
+                    // echo $correctID;
+                }
+            } else {
+                echo "0 results";
+            }
+            
+            $query5 = "SELECT option_List_ID FROM questionoptionlist ORDER BY option_List_ID DESC LIMIT 1;";
+            $result1 = mysqli_query($connection, $query5);
+            if (mysqli_num_rows($result1) > 0) {
+                while($row = mysqli_fetch_assoc($result1)) {
+                    $optionID = $row['option_List_ID'];
+                    // echo $optionID;
+                }
+            } else {
+                echo "0 results";
+            }
+        
+            global $courseid;
+            // echo $courseid;
+            // $question = $_POST['ques'];
+            $question_Gamemode ="MCQ";
+            $current_time = new DateTime();
+            $formatted_time = $current_time->format('Y-m-d H:i:s');
+            
+            $query6 = "INSERT INTO `questionbank`(`course_ID`, `correct_List_ID`, `option_List_ID`, `question`, `question_Gamemode`, `post_Datetime`) VALUES ('$courseid','$correctID', '$optionID', '$questions', '$question_Gamemode', '$formatted_time');";
+            print_r($query6);
+            
+            // print_r($query6);
+            if (mysqli_query($connection, $query6)) {
+                
+                // echo "HAHAHAHAHA SUCCESS";
+                $_SESSION['ansArray'] = array();
+                $_SESSION['pageNumber'] = 1;
+            } else {
+                echo "Error deleting record: " . mysqli_error($connection);
+                echo "Buuuuu";
+            }
+            }
+            echo '<script>alert("Question created successfully")</script>';
+            echo "Number of pages stored: ".count($_SESSION['ansArray'])."<br>";
+    }
+    if ($_POST['cancelBtn'] == "viewQuiz.php") {
+        header("Location: viewQuiz.php");
+    }
+?>

19cf0f4e8f9f1ffd10a542b2ae16606697facfe3
ProtoFaze <110209956+ProtoFaze@users.noreply.github.com>
2023-05-12 10:19:43 +0800

    modified searchUser fixed bug where the no results found prompt does not appear

--- src/views/searchUser.php
+++ src/views/searchUser.php
@@ -61,6 +61,7 @@
             display: flex;
             justify-content: space-between;
             align-items: center;
+            width: 100%;
             height: 50px;
             background-color: var(--box-secondary);
             border-radius: var(--border-radius);
@@ -83,28 +84,13 @@
             border-top-right-radius:0px;
         }
         .search{
-            width: max-content;
+            width: auto;
             gap: 10px;
         }
-        #txtSearch{
-            width: 50%;
-        }
+
         #back,{
             padding: 10px 20px;
             align-self: center; 
-        }
-        @media (max-width: 767px) {
-        .response{
-            flex-direction: column;
-            height: 350px;
-            padding: 15px 10px;
-            justify-content: flex-start;
-        }
-        .search {
-            flex-direction: column;
-            width: 80%;
-            align-self: center;
-        }
         }
     </style>
 </head>
@@ -115,121 +101,118 @@
         if(session_status() == PHP_SESSION_NONE){
             session_start();
         }
-        if(isset($_SESSION['sourceage']) && $_SESSION['sourceage'] == "searchUser" && isset($_SESSION['management_id']) && isset($_SESSION['delete_id'])){
-            unset($_SESSION['sourceage']);
-            unset($_SESSION['delete_id']);
-            unset($_SESSION['management_id']);
-        }
         function detectsearch(){
-            $sql=null;
+            $sql_queries=null;
             if (isset($_POST['display']) && !empty($_POST['txtSearch'])) {
                 $searchby = $_POST['searchby'];
                 $searchterm = $_POST['txtSearch'];
                 if($searchby == "Name"){
-                $sql =
-                    "SELECT `user`.`account_id`, `user`.`user_Type`, `user`.`username`, `teacher`.`tName` AS `name`, `user`.`email`, `user`.`ic`, `user`.`profile_Picture`, `teacher`.`teacher_ID` AS `user_ID` FROM `user` INNER JOIN `teacher` ON `user`.`teacher_ID` = `teacher`.`teacher_ID` 
-                    WHERE `user`.`state` = 1 AND t$searchby LIKE '%$searchterm%';
-                    SELECT `user`.`account_id`, `user`.`user_Type`, `user`.`username`, `parent`.`pName` AS `name`, `user`.`email`, `user`.`ic`, `user`.`profile_Picture`, `parent`.`parent_ID` AS `user_ID` FROM `user` INNER JOIN `parent` ON `user`.`parent_ID` = `parent`.`parent_ID` 
-                    WHERE `user`.`state` = 1 AND p$searchby LIKE '%$searchterm%';
-                    SELECT `user`.`account_id`, `user`.`user_Type`, `user`.`username`, `student`.`sName` AS `name`, `user`.`email`, `user`.`ic`, `user`.`profile_Picture`, `student`.`student_ID` AS `user_ID` FROM `user` INNER JOIN `student` ON `user`.`student_ID` = `student`.`student_ID` 
-                    WHERE `user`.`state` = 1 AND s$searchby LIKE '%$searchterm%';";
+                    $sql_queries = array(
+                    "SELECT `user`.`account_id`, `user`.`user_Type`, `user`.`username`, `teacher`.`tName` AS `name`, `user`.`profile_Picture`, `teacher`.`teacher_ID` AS `user_ID` FROM `user` INNER JOIN `teacher` ON `user`.`teacher_ID` = `teacher`.`teacher_ID` 
+                    WHERE `user`.`state` = 1 AND t$searchby LIKE '%$searchterm%'",
+                    "SELECT `user`.`account_id`, `user`.`user_Type`, `user`.`username`, `parent`.`pName` AS `name`, `user`.`profile_Picture`, `parent`.`parent_ID` AS `user_ID` FROM `user` INNER JOIN `parent` ON `user`.`parent_ID` = `parent`.`parent_ID` 
+                    WHERE `user`.`state` = 1 AND p$searchby LIKE '%$searchterm%'",
+                    "SELECT `user`.`account_id`, `user`.`user_Type`, `user`.`username`, `student`.`sName` AS `name`, `user`.`profile_Picture`, `student`.`student_ID` AS `user_ID` FROM `user` INNER JOIN `student` ON `user`.`student_ID` = `student`.`student_ID` 
+                    WHERE `user`.`state` = 1 AND s$searchby LIKE '%$searchterm%'");
                 }else{
-                $sql = 
+                    $sql_queries = array(
                 //join user table with either student, parent or teacher table
-                "SELECT `user`.`account_id`, `user`.`user_Type`, `user`.`username`, `teacher`.`tName` AS `name`, `user`.`email`, `user`.`ic`, `user`.`profile_Picture`, `teacher`.`teacher_ID` AS `user_ID` FROM `user` INNER JOIN `teacher` ON `user`.`teacher_ID` = `teacher`.`teacher_ID` 
-                WHERE `user`.`state` = 1 AND $searchby LIKE '%$searchterm%';
-                SELECT `user`.`account_id`, `user`.`user_Type`, `user`.`username`, `parent`.`pName` AS `name`, `user`.`email`, `user`.`ic`, `user`.`profile_Picture`, `parent`.`parent_ID` AS `user_ID` FROM `user` INNER JOIN `parent` ON `user`.`parent_ID` = `parent`.`parent_ID` 
-                WHERE `user`.`state` = 1 AND $searchby LIKE '%$searchterm%';
-                SELECT `user`.`account_id`, `user`.`user_Type`, `user`.`username`, `student`.`sName` AS `name`, `user`.`email`, `user`.`ic`, `user`.`profile_Picture`, `student`.`student_ID` AS `user_ID` FROM `user` INNER JOIN `student` ON `user`.`student_ID` = `student`.`student_ID` 
-                WHERE `user`.`state` = 1 AND $searchby LIKE '%$searchterm%';";}
+                "SELECT `user`.`account_id`, `user`.`user_Type`, `user`.`username`, `teacher`.`tName` AS `name`, `user`.`profile_Picture`, `teacher`.`teacher_ID` AS `user_ID` FROM `user` INNER JOIN `teacher` ON `user`.`teacher_ID` = `teacher`.`teacher_ID` 
+                WHERE `user`.`state` = 1 AND $searchby LIKE '%$searchterm%'",
+                "SELECT `user`.`account_id`, `user`.`user_Type`, `user`.`username`, `parent`.`pName` AS `name`, `user`.`profile_Picture`, `parent`.`parent_ID` AS `user_ID` FROM `user` INNER JOIN `parent` ON `user`.`parent_ID` = `parent`.`parent_ID` 
+                WHERE `user`.`state` = 1 AND $searchby LIKE '%$searchterm%'",
+                "SELECT `user`.`account_id`, `user`.`user_Type`, `user`.`username`, `student`.`sName` AS `name`, `user`.`profile_Picture`, `student`.`student_ID` AS `user_ID` FROM `user` INNER JOIN `student` ON `user`.`student_ID` = `student`.`student_ID` 
+                WHERE `user`.`state` = 1 AND $searchby LIKE '%$searchterm%'");}
             }else{
-                $sql = 
-                "SELECT `user`.`account_id`, `user`.`user_Type`, `user`.`username`, `teacher`.`tName` AS `name`, `user`.`email`, `user`.`ic`, `user`.`profile_Picture`, `teacher`.`teacher_ID` AS `user_ID` FROM `user` INNER JOIN `teacher` ON `user`.`teacher_ID` = `teacher`.`teacher_ID` WHERE `user`.`state` = 1;
-                SELECT `user`.`account_id`, `user`.`user_Type`, `user`.`username`, `parent`.`pName` AS `name`, `user`.`email`, `user`.`ic`, `user`.`profile_Picture`, `parent`.`parent_ID` AS `user_ID` FROM `user` INNER JOIN `parent` ON `user`.`parent_ID` = `parent`.`parent_ID` WHERE `user`.`state` = 1;
-                SELECT `user`.`account_id`, `user`.`user_Type`, `user`.`username`, `student`.`sName` AS `name`, `user`.`email`, `user`.`ic`, `user`.`profile_Picture`, `student`.`student_ID` AS `user_ID` FROM `user` INNER JOIN `student` ON `user`.`student_ID` = `student`.`student_ID` WHERE `user`.`state` = 1;";
+                $sql_queries = array(
+                "SELECT `user`.`account_id`, `user`.`username`, `teacher`.`tName` AS `name`, `user`.`user_Type`, `user`.`profile_Picture`, `teacher`.`teacher_ID` AS `user_ID` FROM `user` INNER JOIN `teacher` ON `user`.`teacher_ID` = `teacher`.`teacher_ID` WHERE `user`.`state` = 1",
+                "SELECT `user`.`account_id`, `user`.`username`, `parent`.`pName` AS `name`, `user`.`user_Type`, `user`.`profile_Picture`, `parent`.`parent_ID` AS `user_ID` FROM `user` INNER JOIN `parent` ON `user`.`parent_ID` = `parent`.`parent_ID` WHERE `user`.`state` = 1",
+                "SELECT `user`.`account_id`, `user`.`username`, `student`.`sName` AS `name`, `user`.`user_Type`, `user`.`profile_Picture`, `student`.`student_ID` AS `user_ID` FROM `user` INNER JOIN `student` ON `user`.`student_ID` = `student`.`student_ID` WHERE `user`.`state` = 1");
             }
-            return $sql;
+            return $sql_queries;
         }
-        $query = detectsearch();
-        if (mysqli_multi_query($connection, $query)) {
+        $queries = detectsearch();
         include "../components/nav.php"; ?>
     </header>
     <main>
         <div class="response">
             <button onclick="window.location.href = 'mainpage.php'" id='back' class="flex_button"><span class="material-symbols-outlined">arrow_back_ios</span>Back</button>
             <form class="search" method="post">
-                <h4>Search by:</h4>
-                <select name="searchby" id="dropdowninput">
+                <h5>Search by:<select name="searchby" id="dropdowninput"></h5>
                     <option value="account_ID">Account ID</option>
                     <option value="Name">Legal name</option>
-                    <option value="username">Name</option>
+                    <option value="username">Username</option>
                     <option value="Email">Email</option>
                     <option value="ic">IC number</option>
                 </select>
-                <h4>Enter search data:</h4> <input type="text" name="txtSearch" id="txtSearch">
+                <h5>Enter search data:</h5> <input type="text" name="txtSearch" id="txtSearch">
                 <input class="flex_button" type="submit" value="Search" name= "display">
             </form>
         </div>
     <div class="grid_box">
-<?php
-    do {
-        // get result set and process it
-        if ($result = mysqli_store_result($connection)) {
-            while($row = mysqli_fetch_assoc($result)) {
-                $data = $row['profile_Picture'];
-                $account_ID = $row['account_id'];
-                $user_ID = $row['user_ID'];
-                $user_type = $row['user_Type'];
-                if ($user_type === "Teacher"){
-                    $bg = "var(--bg);";
-                }
-                elseif ($user_type === "Parent"){
-                    $bg = "var(--box-primary);";
-                }
-                elseif ($user_type === "Student"){
-                    $bg = "var(--box-secondary);";
-                }
-                echo
-                    "<form method='POST'>
-                    <button type='submit' name='profile' class='profile' style='background-color: $bg'>
-                    <input type='hidden' name='delete_id' value='".$account_ID."'>
-                    <input type='hidden' name='user_id' value='".$user_ID."'>
-                    <input type='hidden' name='user_type' value='".$user_type."'>";
-                if(empty($data) || $data == NULL){
-                    echo "<img class='prof_pic' src='../../images/anonymousUser.png'>";
-                    }else{
-                    echo "<img class='prof_pic' src='$data'>";
-                    }
-                echo"  
-                    <table style='margin: 0 auto;'>
-                        <tr>
-                            <th>User Type</th>
-                            <td class='uType'>".$user_type."</td>
-                        <tr>
-                            <th>Username</th>
-                            <td>".$row['username']."</td>
-                        </tr>
-                    </table>
-                </button></form>";
-                if(isset($_POST['profile'])){
-                    $_SESSION['sourcepage'] = "searchUser";
-                    $_SESSION['management_id'] = $_POST['user_id'];
-                    $_SESSION['delete_id'] = $_POST['delete_id'];
-                    $type = $_POST['user_type'];
-                    echo "<script>
-                        window.location.href = './Profile$type.php';
-                        </script>";
+<?php $noResults = False;
+foreach ($queries as $query){
+    // echo $query;
+    $result = mysqli_query($connection, $query);
+    if (mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) {
+            // var_dump($row);
+            $data = $row['profile_Picture'];
+            $user_id = $row['account_id'];
+            if (substr($row['user_ID'], 0, 2) == "TC"){
+                $user_type = 'Teacher';
+                // $bg = "rgb(230, 230, 250);";
+                $bg = "var(--bg);";
+            }
+            elseif (substr($row['user_ID'], 0, 2) == "PT"){
+                $user_type = 'Parent';
+                // $bg = "rgb(220, 20, 60);";
+                $bg = "var(--box-primary);";
 
+            }
+            elseif (substr($row['user_ID'], 0, 2) == "ST"){
+                $user_type = 'Student';
+                $bg = "var(--box-secondary);";
+
+            }
+            echo
+                "<form method='POST'>
+                <button type='submit' name='profile' class='profile' style='background-color: $bg'>
+                <input type='hidden' name='delete_id' value='".$user_id."'>";
+            if(empty($data) || $data == NULL){
+                echo "<img class='prof_pic' src='../../images/anonymousUser.png'>";
+                }else{
+                echo "<img class='prof_pic' src='../../images/$data'>";
                 }
-            }mysqli_free_result($result);
+            echo"  
+                <table style='margin: 0 auto;'>
+                    <tr>
+                        <th>User Type</th>
+                        <td class='uType'>".$user_type."</td>
+                    <tr>
+                        <th>Username</th>
+                        <td>".$row['username']."</td>
+                    </tr>
+                </table>
+            </button></form>";
+            if(isset($_POST['profile'])){
+                $_SESSION['sourcepage'] = "searchUser";
+                $_SESSION['delete_id'] = $_POST['delete_id'];
+                echo "<script>
+                    window.location.href = './Profile$user_type.php';
+                    </script>";
+
+            }
         }
-    }while (mysqli_more_results($connection) && mysqli_next_result($connection));
- } else {
-    echo "<h2>0 results</h2>";
-}// save current page
-
-?>
-    </main>
-
+    } else {
+        $noResults = True;
+    }
+}if($noResults == True){echo "<h1>No results found</h1>";}?>
+</main>
+    <!-- SELECT `user`.`account_ID`, `user`.`username`, `teacher`.`tName` AS `name`,`parent`.`pName` AS `name`, `student`.`sName` AS `name`,`user`.`user_Type`, `user`.`profile_Picture`, `teacher`.`teacher_ID` AS `user_ID`,`parent`.`parent_ID` AS `user_ID`,`student`.`student_ID` AS `user_ID` 
+FROM `user` LEFT JOIN `teacher` ON `user`.`teacher_ID` = `teacher`.`teacher_ID` 
+LEFT JOIN `parent` ON `user`.`parent_ID` = `parent`.`parent_ID` 
+LEFT JOIN `student` ON `user`.`student_ID` = `student`.`student_ID` 
+WHERE `user`.`state` = 1; -->
 </body>
 </html>
